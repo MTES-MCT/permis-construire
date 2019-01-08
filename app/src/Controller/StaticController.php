16b272f4f652f019c8e0a82030b7e27a3a0b558c
clemkeirua <clement@keiruaprod.fr>
2019-01-08 20:28:40 +0100

    migration wif -> pc, changement de workflow

--- app/src/Controller/StaticController.php
+++ app/src/Controller/StaticController.php
@@ -9,4 +9,20 @@ class StaticController extends AbstractController
     public function homepage() {
         return $this->render("homepage.html");
     }
+
+    public function qualification1() {
+        return $this->render("qualify/page1.html.twig");
+    }
+
+    public function qualification2() {
+        return $this->render("qualify/page2.html.twig");
+    }
+
+    public function qualification3() {
+        return $this->render("qualify/page3.html.twig");
+    }
+
+    public function resultats() {
+        return $this->render("qualify/resultats.html.twig");
+    }
 }
--- app/src/Security/LoginSuccessHandler.php
+++ app/src/Security/LoginSuccessHandler.php
@@ -22,19 +22,8 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token_name)
     {
-        $roleMapping = [
-            'ROLE_INSTRUCTEUR' => 'route_instructeur_dashboard',
-            'ROLE_MAIRIE' => 'route_mairie_dashboard',
-            'ROLE_ADMIN' => 'easyadmin',
-        ];
+        $redirectionUrl = $this->router->generate('easyadmin');
 
-        $redirectionUrl = $this->router->generate('route_demandeur_dashboard');
-        foreach($token_name->getRoles() as $role) {
-            if(array_key_exists($role->getRole(), $roleMapping)) {
-                $redirectionUrl = $this->router->generate($roleMapping[$role->getRole()]);
-                break;
-            }
-        }
         return new RedirectResponse($redirectionUrl);
     }
 }
