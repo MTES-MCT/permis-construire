fa14a1fc19b817fc52ab317bfeb9f2abffdf57c2
clemkeirua <clement@keiruaprod.fr>
2019-01-09 09:34:13 +0100

    wip steps

--- app/src/Controller/StaticController.php
+++ app/src/Controller/StaticController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Entity\Ville;
+use Symfony\Component\HttpFoundation\Request;
+
 class StaticController extends AbstractController
 {
     public function homepage() {
@@ -11,18 +14,36 @@ class StaticController extends AbstractController
     }
 
     public function qualification1() {
-        return $this->render("qualify/page1.html.twig");
+        $villes = $this->getDoctrine()->getRepository(Ville::class)->findAll();
+
+        return $this->render("qualify/page1.html.twig",
+            ['villes' => $villes]
+        );
     }
 
-    public function qualification2() {
-        return $this->render("qualify/page2.html.twig");
+    public function qualification2(Request $request) {
+        return $this->render("qualify/page2.html.twig",
+            ['ville_id' => $request->get('ville_id', null)]
+        );
     }
 
-    public function qualification3() {
-        return $this->render("qualify/page3.html.twig");
+    public function qualification3(Request $request) {
+        return $this->render(
+            "qualify/page3.html.twig",
+            [
+                'ville_id' => $request->get('ville_id', null),
+                'abf' => $request->get('abf', null),
+            ]
+        );
     }
 
-    public function resultats() {
-        return $this->render("qualify/resultats.html.twig");
+    public function resultats(Request $request) {
+        return $this->render("qualify/resultats.html.twig",
+            [
+                'ville_id' => $request->get('ville_id', null),
+                'abf' => $request->get('abf', null),
+                'projet' => $request->get('projet', null)
+            ]
+        );
     }
 }
