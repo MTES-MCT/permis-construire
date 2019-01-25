bf6f4f60b625a08b753f920050787e765deba012
clemkeirua <clement@keiruaprod.fr>
2019-01-25 10:10:28 +0100

    :sparkles: ajout d'ecrans faq, contact, cgu

--- app/src/Controller/StaticController.php
+++ app/src/Controller/StaticController.php
@@ -10,4 +10,19 @@ class StaticController extends AbstractController
     {
         return $this->render('homepage.html');
     }
+
+    public function cgu()
+    {
+        return $this->render('static/cgu.html.twig');
+    }
+
+    public function faq()
+    {
+        return $this->render('static/faq.html.twig');
+    }
+
+    public function contact()
+    {
+        return $this->render('static/contact.html.twig');
+    }
 }
